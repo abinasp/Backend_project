53e63eea1726ee84e1cbd1040019e17bedc0170d
Abinas Patra <2010abinas@gmail.com>
2019-05-10 16:20:17 +0530

    customer service addded

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+require_once 'routes.php';
+
 define("_INCLUDED", 1);
 if(isset($_GET['cn']) && isset($_GET['fn'])){
-    
+    routes($_GET['cn'], $_GET['fn']);
 }else{
-    echo json_encode(array('success'=>FALSE, 'message'=>"API doesn't exist"));
+    echo json_encode(array('success'=>TRUE, 'message'=>'Welcome'));
 }
 ?>
